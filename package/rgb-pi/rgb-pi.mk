9335316674349bb274c82c97369ad773f304a568
buryplo <antoni.nicolas@gmail.com>
2017-01-29 01:07:19 +0100

    Add Moonlight-Embedded Fix for libCEC 3 Fix RGB-PI repo

--- package/rgb-pi/rgb-pi.mk
+++ package/rgb-pi/rgb-pi.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RGB_PI_VERSION = 02ff5a085e8aa641c76677059904c48bf02ec226
+RGB_PI_VERSION = 9fa8fb4996bd5c98038a6c77dc6f314f78ac5858
 #RGB_PI_SITE = git://github.com/mortaca/RGB-Pi
 RGB_PI_SITE = $(call github,mortaca,rgb-pi,$(RGB_PI_VERSION))
 RGB_PI_DEPENDENCIES += rpi-firmware
